30dcf26b51d4bb4b57b4393a8995d2623c4db933
Walter Smuts <smuts.walter@gmail.com>
2020-11-18 14:49:56 +0200

    Fixed double free memory bug and restructured code
    There's still a strange error where when I spam messages to the server,
    they get missed and only picked up when I send more. I've put print
    statements to help investigate what's going on.
    
    Currently it doesn't seem like any messages are being dropped. Just that
    they are ignored until another batch of messages arrive. Then the server
    proptly forgets about another few.

--- cpp-protobuf/main.cpp
+++ cpp-protobuf/main.cpp
@@ -15,34 +15,16 @@
 #include <sys/un.h>
 #include <unistd.h>
 
-int main() {
+void sendSumTask(int a, int b, int data_socket) {
 	// Setup task
-	SumTask sumTask;
-	sumTask.set_val1(1);
-	sumTask.set_val2(1);
+	SumTask *sumTask = new SumTask();
+	sumTask->set_val1(a);
+	sumTask->set_val2(b);
 	ArithmeticTask wrapper;
-	wrapper.set_allocated_sum_task(&sumTask);
+	wrapper.set_allocated_sum_task(sumTask);
 	size_t size = wrapper.ByteSize();
 	char c[size];
 	wrapper.SerializeToArray(&c, size);
-
-	// Create socket
-	int data_socket = socket(AF_UNIX, SOCK_DGRAM, 0);
-	if (data_socket == -1) {
-		perror("socket");
-		exit(EXIT_FAILURE);
-	}
-
-	// Connect socket to socket address
-        struct sockaddr_un addr;
-	addr.sun_family = AF_UNIX;
-	strncpy(addr.sun_path, "/tmp/rust-ipc.sock", sizeof(addr.sun_path) - 1);
-	int ret = connect(data_socket, (const struct sockaddr *) &addr, sizeof(struct sockaddr_un));
-	if (ret == -1) {
-		fprintf(stderr, "The server is down.\n");
-		exit(EXIT_FAILURE);
-	}
-
 	ssize_t written = write(data_socket, c, size);
 
 	if (written != size) {
@@ -52,8 +34,30 @@ int main() {
 		std::cout << "errno: " << errno << std::endl;
 		exit(EXIT_FAILURE);
 	}
+}
+
+int main() {
+	// Verify that the version of the library that we linked against is
+	// compatible with the version of the headers we compiled against.
+	GOOGLE_PROTOBUF_VERIFY_VERSION;
+	// Create socket
+	int data_socket = socket(AF_UNIX, SOCK_DGRAM, 0);
+	if (data_socket == -1) {
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+
+	// Connect socket to socket address
+	struct sockaddr_un addr;
+	addr.sun_family = AF_UNIX;
+	strncpy(addr.sun_path, "/tmp/rust-ipc.sock", sizeof(addr.sun_path) - 1);
+	int ret = connect(data_socket, (const struct sockaddr *) &addr, sizeof(struct sockaddr_un));
+	if (ret == -1) {
+		fprintf(stderr, "The server is down.\n");
+		exit(EXIT_FAILURE);
+	}
+	for (int i=0; i < 200; i++) sendSumTask(1, i+1, data_socket);
 
 	close(data_socket);
-	exit(EXIT_SUCCESS);
 	return 0;
 }
--- rust-protobuf/src/main.rs
+++ rust-protobuf/src/main.rs
@@ -6,13 +6,13 @@ use protos::arithmetic::ArithmeticResponse;
 use protos::arithmetic::ArithmeticTask_oneof_subtask;
 
 use protobuf::parse_from_bytes;
-use protobuf::Message;
 
 use nix::sys::socket::recvmsg;
 use nix::sys::socket::MsgFlags;
 use nix::sys::uio::IoVec;
 
 use mio::{Poll, Events,Interest, Token};
+use mio::event::Event;
 use mio::net::UnixDatagram;
 
 use std::path::Path;
@@ -31,34 +31,40 @@ fn main() {
     poll.registry().register(&mut slab[token], Token(token), Interest::READABLE).unwrap();
 
     loop {
+        println!("Polling...");
         poll.poll(&mut events, None).unwrap();
         for event in &events {
-            let socket =  &slab[usize::from(event.token())];
-            let mut buff = [0u8;  2048];
-            let iov = [IoVec::from_mut_slice(&mut buff[..])];
-            if let Ok(msg) = recvmsg(socket.as_raw_fd(), &iov, None, MsgFlags::empty()) {
-                let vec = buff[0..msg.bytes].to_vec();
-                let task = parse_from_bytes::<ArithmeticTask>(&vec).unwrap();
-                let response = match task.subtask.unwrap() {
-                    ArithmeticTask_oneof_subtask::sum_task(task) => handleSumTask(task),
-                    ArithmeticTask_oneof_subtask::diff_task(task) => handleDiffTask(task),
-                };
-                println!("{:?}", response);
-            } else {
-                println!("Got weird event");
-            }
+            handle_event(&event, &slab);
         }
     }
 }
 
-fn handleSumTask(task :SumTask) -> ArithmeticResponse {
+fn handle_event(event: &Event, slab: &Slab<UnixDatagram>) {
+    println!("Handling event: {:?}", event);
+    let socket =  &slab[usize::from(event.token())];
+    let mut buff = [0u8;  2048];
+    let iov = [IoVec::from_mut_slice(&mut buff[..])];
+    if let Ok(msg) = recvmsg(socket.as_raw_fd(), &iov, None, MsgFlags::empty()) {
+        let vec = buff[0..msg.bytes].to_vec();
+        let task = parse_from_bytes::<ArithmeticTask>(&vec).unwrap();
+        let response = match task.subtask.unwrap() {
+            ArithmeticTask_oneof_subtask::sum_task(task) => handle_sum_task(task),
+            ArithmeticTask_oneof_subtask::diff_task(task) => handle_diff_task(task),
+        };
+        println!("{:?}", response);
+    } else {
+        println!("Got weird event");
+    }
+}
+
+fn handle_sum_task(task: SumTask) -> ArithmeticResponse {
     println!("SumTask: {:?}", task);
     let mut response = ArithmeticResponse::new();
     response.set_answer(task.val1 + task.val2);
     return response;
 }
 
-fn handleDiffTask(task :DiffTask) -> ArithmeticResponse {
+fn handle_diff_task(task: DiffTask) -> ArithmeticResponse {
     println!("DiffTask: {:?}", task);
     let mut response = ArithmeticResponse::new();
     response.set_answer(task.val1 - task.val2);
